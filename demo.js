75123aec6cbf9662df480ad4ac031fbe749baa84
Robert Egnacheski <robert@egnacheski.com>
2015-10-09 18:35:54 -0400

    initial CSS, HTML and JS

---
+++ demo.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+applyTextShadow = function(element, shdw){
+
+console.log(element + " " + shdw);
+$( element ).css( "text-shadow",  shdw);
+$( element ).css( "color",  "rgba(0, 0, 0, .2)");
+
+}
